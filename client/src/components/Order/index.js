e4b0ec0a27185b0a6e56bb4b5e2bbf59d6f1fd93
srahman9 <32348687+srahman9@users.noreply.github.com>
2022-04-30 09:52:40 -0400

    update

--- client/src/components/Order/index.js
+++ client/src/components/Order/index.js
@@ -1,52 +1,11 @@
-<<<<<<< HEAD
-import React, {useState} from 'react';
-import Modal from '../Modal';
-
-// const customStyles = {
-//   content: {
-//     top: '50%',
-//     left: '50%',
-//     right: 'auto',
-//     bottom: 'auto',
-//     marginRight: '-50%',
-//     transform: 'translate(-50%, -50%)',
-//   },
-// };
-
-
-const Order = () => {
-
-const [show, setShow] = useState(false)
-
-  // let orders = [
-  //   {category: "This Week Orders",
-  //   data: "something"},
-
-  //   {category: "Last Week Orders",
-  //   data: "something"},
-
-  //   {category: "Past Orders",
-  //   data: "something"},
-  // ]
-
-  return (
-    <div className="p-4">
-      <button onClick={() => setShow(true)}>Show Modal</button>
-      <Modal onClose={() => setShow(false)} show={show}/>
-=======
 import React from 'react';
 
 const Order = () => {
   return (
     <div className="p-4">
         Current and Past Orders
->>>>>>> aba9a9c3551147f2b316545ff2d5704b2b1db832
     </div>
   );
 };
 
-<<<<<<< HEAD
 export default Order;
-=======
-export default Order;
->>>>>>> aba9a9c3551147f2b316545ff2d5704b2b1db832
--- client/src/pages/Landing.js
+++ client/src/pages/Landing.js
@@ -1,17 +1,7 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-<<<<<<< HEAD
-import farfar from '../assets/images/Badges/farfar.png'
-import gwBestMealAward from '../assets/images/Badges/gwBestMealAward.png'
-import neverland from '../assets/images/Badges/neverland.png'
-
 let ReactRotatingText = require('react-rotating-text');
 
-
-=======
-let ReactRotatingText = require('react-rotating-text');
-
->>>>>>> aba9a9c3551147f2b316545ff2d5704b2b1db832
 const Landing = () => {
 
     const meals = ["VEGETARIAN", "PESCETARIAN", "MEATLOVER"]
@@ -25,30 +15,6 @@ const Landing = () => {
         return (
             <div className='LandingPage'>
                 <div className='intro-center'>
-<<<<<<< HEAD
-                    <div className='intro-holder'>
-                        <div className="intro col-8">
-                            <div className='title'>
-                                <p>BRYSS</p>
-                            </div>
-                            <div className="intro-text">
-                                DELICIOUS AND HEALTHY
-                                <br/>MEALS FOR <span  className="rotateText" ><ReactRotatingText items={meals}/></span>
-                                <br/>FROM THE KITCHEN TO YOUR TABLE
-                            </div>
-                            <div className='inspiration'>
-                                <p className='quote col-12'>"We all eat, and it would be a sad waste of opportunity to eat badly."<br/>by ANNE THOMAS </p>
-                                <p className='promise'>
-                                    We only treats the best and freshest ingredients with carefully selected excellent chefs.<br/> Start your weekly meal plan now!
-                                </p>
-
-                            </div>
-                        </div>
-                        <div className="sideCard col-4">
-                            <div className='startUp'>
-                                <div className="center">
-                                    <button className="SignUp"><Link to="/signup">Sign Up</Link></button>
-=======
                     <div className='title'><p>BRYSS</p></div>
                     <div className='intro-holder'>
                         <div className="intro col-9">
@@ -64,26 +30,11 @@ const Landing = () => {
                                 <div className="center">
                                     <button className="SignUp"><Link to="/signup">Sign Up</Link></button>
                                     <p2>or</p2>
->>>>>>> aba9a9c3551147f2b316545ff2d5704b2b1db832
                                     <button className="Login"><Link to="/login">Login</Link></button>
                                 </div>
                             </div>
                         </div>
                     </div>
-<<<<<<< HEAD
-                    <div className="badge-holder flex-row justify-space-around align-center">
-                                <div className='col-3'>
-                                    <img className='badge' src={farfar} alt="Farfar badge"/>
-                                </div>
-                                <div className='col-3'>
-                                    <img className='badge' src={gwBestMealAward} alt="Farfar badge"/>
-                                </div>
-                                <div className='col-3'>
-                                    <img className='badge' src={neverland} alt="Farfar badge"/>
-                                </div>
-                    </div>
-=======
->>>>>>> aba9a9c3551147f2b316545ff2d5704b2b1db832
                 </div>
             </div>
         )
