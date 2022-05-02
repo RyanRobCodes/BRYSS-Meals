60bb91b9913607bf0af35b4e159366f501c239a3
srahman9 <32348687+srahman9@users.noreply.github.com>
2022-05-02 12:26:04 -0400

    update

--- client/src/utils/API.js
+++ client/src/utils/API.js
@@ -58,20 +58,35 @@ export const deleteMeal = (bookId, token) => {
 //   );
 // };
 
-const options = {
-  method: "GET",
-  headers: {
-    "X-RapidAPI-Host": "edamam-edamam-nutrition-analysis.p.rapidapi.com",
-    "X-RapidAPI-Key": "82af9eb5a08fc44832b441adc629688f",
-  },
-};
+const { NutritionAnalysisClient } = require('edamam-api');
+ 
+(async () => {
+ 
+  const client = new NutritionAnalysisClient({
+    appId: '1c60c625',
+    appKey: '82af9eb5a08fc44832b441adc629688f'
+  });
+ 
+  const results = await client.search({ query: 'Chicken' });
+ 
+})();
 
-fetch(
-  "https://edamam-edamam-nutrition-analysis.p.rapidapi.com/api/nutrition-data?ingr=1%20large%20apple",
-  options
-)
-  .then((response) => response.json())
-  .then((response) => console.log(response))
-  .catch((err) => console.error(err));
+
+
+// const options = {
+//   method: "GET",
+//   headers: {
+//     "X-RapidAPI-Host": "edamam-edamam-nutrition-analysis.p.rapidapi.com",
+//     "X-RapidAPI-Key": "82af9eb5a08fc44832b441adc629688f",
+//   },
+// };
+
+// fetch(
+//   "https://edamam-edamam-nutrition-analysis.p.rapidapi.com/api/nutrition-data?ingr=1%20large%20apple",
+//   options
+// )
+//   .then((response) => response.json())
+//   .then((response) => console.log(response))
+//   .catch((err) => console.error(err));
 
 //   www.themealdb.com/api/json/v1/1/lookup.php?i=52772
