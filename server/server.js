d864b849336518329856a81281fa104ba8650faa
srahman9 <32348687+srahman9@users.noreply.github.com>
2022-05-03 15:45:14 -0400

    update currency

--- server/server.js
+++ server/server.js
@@ -28,7 +28,7 @@ app.post("/create-payment-intent", async (req, res) => {
   // Create a PaymentIntent with the order amount and currency
   const paymentIntent = await stripe.paymentIntents.create({
     amount: calculateOrderAmount(items),
-    currency: "eur",
+    currency: "USD",
     automatic_payment_methods: {
       enabled: true,
     },
